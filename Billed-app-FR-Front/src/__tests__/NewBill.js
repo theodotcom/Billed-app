6e4709369a14dea493dd6f8caba9c875ec024b1b
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-08-29 20:41:02 +0200

    higlighted mail icon test

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,39 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+ import { 
+  getByTestId,
+  getAllByTestId,
+  getAllByAltText,
+  getByRole,
+  toHaveClass
+ } from '@testing-library/dom'
+ import userEvent from '@testing-library/user-event'
+import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { ROUTES_PATH, ROUTES} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
 
+
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then mail icon in vertical layout should be highlighted", async () => {
+    
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const mailIcon = screen.getByTestId('icon-mail')
+      expect(mailIcon.classList.contains('active-icon')).toBeTruthy();
     })
   })
 })
